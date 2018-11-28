217414abb3f6aeb4f6466e14dd4389a7f345b9a5
zyy422 <422535451@qq.com>
2018-11-28 10:20:08 +0800

    rev.11.28

--- double_list/dLList.h
+++ double_list/dLList.h
@@ -1,15 +1,21 @@
 #ifndef DLLIST_H_
 #define DLLIST_H
 
+template<typename T>
+class DLList;			//declaration of template class (must exist)
+
 template <typename T>
 class DLLNode
 {
 public:
+	//friend class DLList<T>;		//friend class 
 	DLLNode() : prev(0), next(0) {};
 	DLLNode(T el, DLLNode *plast = 0, DLLNode *pnext = 0) : prev(plast), next(pnext), info(el) {};
+	friend void DLList<T>::showAll();	//friend class function
 private:
 	DLLNode *prev, *next;
 	T info;
+
 };
 
 template <typename T>
@@ -19,11 +25,13 @@ public:
 	DLList(): head(0), tail(0){};
 	void addToTail(T el);		//insert data after tail
 	void addToHead(T el);		//insert data before head
+	void showAll();
 private:
 	DLLNode<T> *head, *tail;
 };
 
 //***********template class declare and definition must be exist one file together***//
+
 template<typename T>
 void DLList<T>::addToTail(T el)
 {
@@ -45,4 +53,12 @@ void DLList<T>::addToHead(T el)
 	head = new DLLNode<T>(el, 0, head);
 }
 
+template<typename T>
+void DLList<T>::showAll()
+{
+	for (DLLNode<T> *tmp = head ; tmp != tail->next; tmp = tmp->next)
+	{
+		cout << tmp->info << endl;
+	}
+}
 #endif DLLIST_H
--- double_list/main.cpp
+++ double_list/main.cpp
@@ -10,5 +10,8 @@ int main()
 	DLList<int> my_list;
 	my_list.addToTail(100);
 	my_list.addToHead(200);
+	my_list.addToHead(300);
+	my_list.addToHead(400);
+	my_list.showAll();
 	return 0;
 }
